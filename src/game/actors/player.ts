82c8db63405071704ab1b3fa9f67c8ecccb0f503
Alex Wain <waino67@gmail.com>
2017-08-26 04:49:10 +0100

    Full player movement

--- src/game/actors/player.ts
+++ src/game/actors/player.ts
@@ -3,7 +3,7 @@ import { Engine, Actor, Color, Input, Vector } from 'excalibur';
 export class Player extends Actor {
 	private static readonly SIZE: number = 40;
 
-	private readonly movementSpeed = 100;
+	private readonly movementSpeed = 200;
 
 	constructor() {
 		super(-(Player.SIZE / 2), -(Player.SIZE / 2), Player.SIZE, Player.SIZE, Color.White);
@@ -14,19 +14,42 @@ export class Player extends Actor {
 	}
 
 	public update(engine: Engine, delta: number) {
-		if (engine.input.keyboard.isHeld(Input.Keys.W)) {
-			this.moveForward();
-		} else {
-			this.stopMoving();
-		}
+		this.HandleMovement(engine);
 		super.update(engine, delta);
 	}
 
-	private moveForward() {
-		this.vel.setTo(0, -this.movementSpeed);
+	private HandleMovement(engine: Engine) {
+		this.vel.setTo(0, 0);
+		if (engine.input.keyboard.isHeld(Input.Keys.W)) {
+			this.moveForward();
+		}
+		if (engine.input.keyboard.isHeld(Input.Keys.A)) {
+			this.moveLeft();
+		}
+		if (engine.input.keyboard.isHeld(Input.Keys.S)) {
+			this.moveBackward();
+		}
+		if (engine.input.keyboard.isHeld(Input.Keys.D)) {
+			this.moveRight();
+		}
+		if (!this.vel.equals(Vector.Zero)) {
+			this.vel = this.vel.normalize().scale(this.movementSpeed);
+		}
 	}
 
-	private stopMoving() {
-		this.vel.setTo(0, 0);
+	private moveForward() {
+		this.vel = this.vel.add(new Vector(0, -this.movementSpeed));
+	}
+
+	private moveLeft() {
+		this.vel = this.vel.add(new Vector(-this.movementSpeed, 0));
+	}
+
+	private moveRight() {
+		this.vel = this.vel.add(new Vector(this.movementSpeed, 0));
+	}
+
+	private moveBackward() {
+		this.vel = this.vel.add(new Vector(0, this.movementSpeed));
 	}
 }
