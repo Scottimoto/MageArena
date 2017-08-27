830e1534c18a1a74a4c80b1603e2ecf3badefe23
Alex Wain <waino67@gmail.com>
2017-08-28 00:40:35 +0100

    Fixed shooting by not using this in collision handlers and handling all parts in a single actor's collision handler. Also added auto-firing

--- src/game/actors/projectile.ts
+++ src/game/actors/projectile.ts
@@ -4,13 +4,14 @@ import { Monster } from "./monster";
 export class Projectile extends Actor {
 	private startTime: number;
 	private readonly angle: number;
+	private actorsDamaged: number[];
 
 	private readonly speed: number;
 	private readonly range: number;
 	
 	public readonly damage: number;
 	
-	constructor(startX: number, startY: number, angle: number) {
+	constructor(startX: number, startY: number, angle: number, damage) {
 		const size: number = 10;
 
 		super(startX, startY, size, size, Color.Black);
@@ -25,6 +26,7 @@ export class Projectile extends Actor {
 	public onInitialize(engine: Engine) {
 		this.on("collision", this.onCollision);
 		this.vel = Vector.fromAngle(this.angle).normalize().scale(this.speed);
+		this.actorsDamaged = [];
 		this.startTime = Date.now();
 	}
 
@@ -36,8 +38,11 @@ export class Projectile extends Actor {
 	}
 
 	private onCollision(e: CollisionEvent): void {
-		if(e.other instanceof(Monster)) {
-			this.kill();
+		const projectile = e.actor as Projectile;
+		if(e.other instanceof(Monster) && projectile.actorsDamaged.indexOf(e.other.id) === -1) {
+			e.other.damage(projectile.damage);
+			projectile.actorsDamaged.push(e.other.id);
+			projectile.kill();
 		}
 	}
 }
