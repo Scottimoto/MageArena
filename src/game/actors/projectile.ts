7f1356450efc8985200d2dc43684a2e1c56df68c
Alex Wain <waino67@gmail.com>
2017-08-27 08:29:33 +0100

    Added range to projectiles

--- src/game/actors/projectile.ts
+++ src/game/actors/projectile.ts
@@ -1,19 +1,31 @@
 import { Engine, Actor, Color, Vector } from 'excalibur';
 
 export class Projectile extends Actor {
-	private readonly speed: number;
+	private startTime: number;
 	private readonly angle: number;
+
+	private readonly speed: number;
+	private readonly range: number;
 	
 	constructor(startX: number, startY: number, angle: number) {
 		const size: number = 10;
 
 		super(startX, startY, size, size, Color.Black);
 
-		this.speed = 1000;
 		this.angle = angle;
+		this.speed = 1000;
+		this.range = 800;
 	}
 
 	public onInitialize(engine: Engine) {
 		this.vel = Vector.fromAngle(this.angle).normalize().scale(this.speed);
+		this.startTime = Date.now();
+	}
+
+	public update(engine: Engine, delta: number) {
+		if (Date.now() - this.startTime > this.range) {
+			this.kill();
+		}
+		super.update(engine, delta);
 	}
 }
